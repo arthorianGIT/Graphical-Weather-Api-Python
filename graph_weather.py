e2244c7db2f40d756206e84dc5395823b8d5f9df
neverappealed <honeyrot222@gmail.com>
2025-02-16 19:42:19 +0300

    added label with name of programm

--- graph_weather.py
+++ graph_weather.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 root = ctk.CTk()
-root.title('Weather Api Soft')
+root.title('Window')
 root.geometry('900x600')
 root.resizable(False, False)
 root._set_appearance_mode('dark')
@@ -12,14 +12,21 @@ root._set_appearance_mode('dark')
 load_dotenv('.env')
 api_key = os.getenv('my_api_key')
 
-frame = ctk.CTkFrame(root, fg_color='#2C2C2C', bg_color='#2C2C2C', height=600)
+frame = ctk.CTkFrame(root, fg_color='#2C2C2C', bg_color='#2C2C2C', height=600, width=250)
 frame.place(x=0, y=0)
+
+label = ctk.CTkLabel(root, text='Weather API', fg_color='#2C2C2C', bg_color='#2C2C2C', text_color='white',
+                     width=60, height=30, font=('Helvetica', 26, 'bold'), anchor='center')
+label.place(x=40, y=45)
+label2 = ctk.CTkLabel(root, text='made by neverappealed', fg_color='#2C2C2C', bg_color='#2C2C2C', anchor='center',
+                      text_color='white', font=('Helvetica', 14, 'bold'), height=10, width=60)
+label2.place(x=34, y=85)
 
 save_text = ctk.StringVar()
 city_entry = ctk.CTkEntry(root, placeholder_text='Enter here your city', width=300, height=40, font=('Arial', 17),
                           textvariable=save_text, bg_color='gray', fg_color='gray', text_color='white', corner_radius=5,
                           border_width=4, placeholder_text_color='white')
-city_entry.place(y=230, x=300)
+city_entry.place(y=200, x=410)
 
 
 
