4ae7f56ccc8b5f6451d7205e70819e48bdf2b949
neverappealed <honeyrot222@gmail.com>
2025-02-16 19:25:18 +0300

    rebuild of design

--- graph_weather.py
+++ graph_weather.py
@@ -12,14 +12,8 @@ root._set_appearance_mode('dark')
 load_dotenv('.env')
 api_key = os.getenv('my_api_key')
 
-frame = ctk.CTkFrame(root, fg_color='gray', width=150, height=40, bg_color='gray')
-frame.place(x=410, y=75)
-frame2 = ctk.CTkFrame(root, fg_color='gray', width=150, height=40, bg_color='gray')
-frame2.place(x=330, y=75)
-
-label = ctk.CTkLabel(root, text='This is Api Weather GUI', bg_color='gray', width=130, height=40,
-                      font=('Helvetica', 17, 'bold'), anchor='center', text_color='#00A3FF')
-label.place(x=350, y=75)
+frame = ctk.CTkFrame(root, fg_color='#2C2C2C', bg_color='#2C2C2C', height=600)
+frame.place(x=0, y=0)
 
 save_text = ctk.StringVar()
 city_entry = ctk.CTkEntry(root, placeholder_text='Enter here your city', width=300, height=40, font=('Arial', 17),
