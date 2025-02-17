783ffad1a7e08951ceeb62e1d2544fb958093efd
neverappealed <honeyrot222@gmail.com>
2025-02-17 09:34:27 +0300

    writed final script

--- graph_weather.py
+++ graph_weather.py
@@ -12,6 +12,23 @@ root._set_appearance_mode('dark')
 load_dotenv('.env')
 api_key = os.getenv('my_api_key')
 
+def weather_script():
+    city = save_text.get()
+    params = {
+        'q': city,
+        'appid': api_key
+    }
+    response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        weather = data['weather'][0]['description']
+        temperature = data['main']['temp']
+
+        weather_label.configure(text=f'Weather description: {weather}\nTemperature: {temperature - 273.15:.2f}')
+    else:
+        weather_label.configure(text=f'Error: status code response {response.status_code}')
+
 frame = ctk.CTkFrame(root, fg_color='#2C2C2C', bg_color='#2C2C2C', height=600, width=250)
 frame.place(x=0, y=0)
 
@@ -26,10 +43,14 @@ save_text = ctk.StringVar()
 city_entry = ctk.CTkEntry(root, placeholder_text='Enter here your city', width=300, height=40, font=('Arial', 17),
                           textvariable=save_text, bg_color='gray', fg_color='gray', text_color='white', corner_radius=5,
                           border_width=4, placeholder_text_color='white')
-city_entry.place(y=200, x=410)
-weather_button = ctk.CTkButton(root, text='Show weather', width=140, height=35, bg_color='gray', font=('Helvetica', 15))
-weather_button.place(x=488, y=270)
-
-
+city_entry.place(y=260, x=410)
+weather_button = ctk.CTkButton(root, text='Show weather', width=140, height=35, bg_color='#1C4AA8',
+                                font=('Helvetica', 16, 'italic'), fg_color='#1C4AA8', command=weather_script)
+weather_button.place(x=488, y=330)
+weather_frame = ctk.CTkFrame(root, width=300, height=80, fg_color='gray', bg_color='gray')
+weather_frame.place(x=410, y=150)
+weather_label = ctk.CTkLabel(root, text='There will be weather', text_color='black', fg_color='gray', bg_color='gray',
+                             font=('Helvetica', 16, 'bold'), anchor='center')
+weather_label.place(x=410, y=156)
 
 root.mainloop()
